2170131f81883a4606b4051cee7ff9be711044c6
Alen Antony <alenantonyme@gmail.com>
2023-10-29 15:41:32 +0530

    Changed to mysql

--- database.js
+++ database.js
@@ -1,15 +1,25 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize({
-    dialect: process.env.DB_DIALECT,
-    storage: process.env.DB_STORAGE,
-    define: {
+//const sequelize = new Sequelize({
+    //dialect: process.env.DB_DIALECT,
+    //storage: process.env.DB_STORAGE,
+    //define: {
+        //timestamps: false,
+        //freezeTableName: true,
+    //},
+    //logging: false,
+  //})
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  define: {
         timestamps: false,
         freezeTableName: true,
     },
     logging: false,
-  })
+});
 
 
 module.exports=sequelize
